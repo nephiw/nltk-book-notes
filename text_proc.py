7a0df678580a79ff8637e37ef864d376bbbf2ef5
Nephi Wright <nephiw@gmail.com>
2016-07-12 21:22:15 -0600

    Continue work on Chapter 2 and update gitignore

---
+++ text_proc.py
@@ -0,0 +1,24 @@
+# A simple method for making nouns plural
+def plural (word):
+    if word.endswith('y'):
+        return word[:-1] + 'ies'
+    elif word[-1] in 'sx' or word[-2:] in ['sh', 'ch']:
+        return word + 'es'
+    elif word.endswith('an'):
+        return word[:-2] + 'en'
+    else: 
+        return word + 's'
+
+def unusual_word (text):
+    import nltk
+    text_vocab = set(w.lower() for w in text if w.isalpha())
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    unusual = text_vocab - english_vocab
+    return sorted(unusual)
+
+def content_fraction (text):
+    import nltk
+    stopwords = nltk.corpus.stopwords.words('english')
+    content = [w for w in text if w.lower() not in stopwords]
+    return len(content) / len(text)
+
